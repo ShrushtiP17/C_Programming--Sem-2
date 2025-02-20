f3ed83d603bcfeb3d84de77fe65248f6debf8330
Shrushti.Phatate <shrushtiphatate37@gmail.com>
2025-02-20 13:10:13 +0530

    Update ROOTS OF A QUADRATIC EQUATION,FACTORIAL.c

--- ROOTS OF A QUADRATIC EQUATION.c
+++ ROOTS OF A QUADRATIC EQUATION.c
@@ -2,6 +2,7 @@
 #include <math.h>
 int main()
 {
+//Roots of a quadratic equation
 float a,b,c,det,img,real;
 float root1,root2,sq,sqr;
 printf("Enter First Number:");
@@ -31,5 +32,20 @@ printf("Roots of this Quadratic Equation are: %f + i%f\n and \n%f - i%f ",real,i
         root1=root2=(-b)/(2.0*a);
         printf("Roots of this Quadratic Equation are: %f\n and \n%f (we have repeated roots)",root1,root2);
     }
+
+
+
+
+    //Factorial of a number
+    int a,b,c=1;
+    printf("Enter a number: ");
+scanf("%d",&a);
+
+    while(a>0)
+        {
+        c=c*a;
+        a--;
+        }
+    printf("\nfactorial: %d\n",c)
 return 0;
 }
